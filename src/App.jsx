8adf40cee6c7ed353438bc7ee66bd17a70a19639
Wwright91 <wisdomwright-martin@pursuit.org>
2024-07-01 20:59:15 -0400

    Set up routes
    1. Installed react-router-dom
    2. Created Components and Pages
    3. Set up paths and elements for each route
    4. Created boilerplates for all Pages and Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+
+import Edit from './Pages/Edit'
+import FourOFour from './Pages/FourOFour'
+import Home from './Pages/Home'
+import Index from './Pages/Index'
+import New from './Pages/New'
+import Show from './Pages/Show'
+
+import NavBar from './Components/NavBar'
 
 const API = import.meta.env.VITE_API_URL
 
@@ -17,8 +25,21 @@ function App() {
   }, [])
 
   return (
-    <>
-    </>
+    <div>
+      <Router>
+        <NavBar/>
+        <main>
+          <Routes>
+            <Route path='/' element={ <Home/>} />
+            <Route path='/workouts' element={<Index/> } />
+            <Route path='/workouts/:id' element={<Show/> } />
+            <Route path='/workouts/:id/edit' element={ <Edit/>} />
+            <Route path='/workouts/new' element={<New/> } />
+            <Route path='*' element={ <FourOFour/>} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
   )
 }
 
